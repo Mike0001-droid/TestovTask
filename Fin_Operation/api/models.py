980f811fb1dfaedb56c62d8695a8f95b507a3b4e
mike <majorov-2004@list.ru>
2024-07-15 13:52:39 +0500

    сделать логику с параметром account_balance

--- Fin_Operation/api/models.py
+++ Fin_Operation/api/models.py
@@ -12,15 +12,26 @@ class BankAccount(models.Model):
         verbose_name = 'Счёт'
         verbose_name_plural = 'Счета'
 
+    
 
+        
 class Transactions(models.Model):
     account = models.ForeignKey(BankAccount, verbose_name='Банковский счёт', on_delete=models.CASCADE, related_name="account")
     date = models.DateField('Дата операции')
     amount = models.FloatField('Сумма операции')
-
+    account_balance = models.IntegerField('Баланс счёта на момент совершения операции', null=True, blank=True)
+        
     def __str__(self):
         return f"{self.date} - {self.account}"
     
+    def save(self, *args, **kwargs):
+        try:
+            last_instance = Transactions.objects.last()
+            self.account_balance = last_instance.account_balance + self.amount
+        except:
+            self.account_balance = self.amount
+        super(Transactions, self).save(*args, **kwargs) 
+
     class Meta:
         verbose_name = 'Операция'
         verbose_name_plural = 'Операции'
